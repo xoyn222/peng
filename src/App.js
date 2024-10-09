1933f7284bbedc802370f7eb33b1ece651a2a7ca
xoyn222 <kostuk186@gmail.com>
2024-10-09 17:10:07 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import ImageSlider from './components/ImageSlider';
+import ClothingCard from './components/ClothingCard';
+import ClotheData from './components/ClotheData';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Sidebar />
+                <Routes>
+                    <Route path="/" element={<ImageSlider />} />
+                    {ClotheData.map(item => (
+                        <Route
+                            key={item.id}
+                            path={`/clothing/${item.id}`}
+                            element={<ClothingCard clothing={item} />}
+                        />
+                    ))}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
